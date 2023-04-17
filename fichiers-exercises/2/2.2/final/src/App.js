ace9c42fb41068a03566b7095913816ee96b8bf3
SwiftDev <sandyludosky@gmail.com>
2023-04-17 14:20:55 +0000

    03_02e

--- fichiers-exercises/2/2.2/final/src/App.js
+++ fichiers-exercises/2/2.2/final/src/App.js
@@ -55,8 +55,35 @@ const BottomButtons = ({ increment, decrement }) => {
   );
 };
 
-const initialState = {};
-function reducer() {}
+const initialState = {
+  count: 0,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return {
+        count: state.count + 1,
+      };
+    case "decrement":
+      return {
+        count: state.count - 1,
+      };
+    case "incrementBy10":
+      return {
+        count: state.count + 10,
+      };
+    case "decrementBy10":
+      return {
+        count: state.count - 10,
+      };
+    case "reset":
+      return {
+        count: 0,
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
   // useState
